13c530f0d9d7dd3699e7ab0e6b152db6a79f8a6c
agustian-nurilham <44261903+agustian-nurilham@users.noreply.github.com>
2018-10-18 19:23:53 +0700

    initial portfolio
    -

---
+++ js/script.js
@@ -0,0 +1,48 @@
+M.AutoInit();
+//sidebar navigation on mobile
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+//floating action button
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, options);
+});
+
+//paralax event
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.parallax');
+var instances = M.Parallax.init(elems, options);
+});
+
+//dropdown comp
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
+
+//collapsible
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+//tooltips
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, options);
+});
+
+//carousel
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+//materialbox
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, options);
+});
